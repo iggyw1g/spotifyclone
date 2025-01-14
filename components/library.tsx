be4c956c07651c6242bf4079ea1085a87500c7c8
Alisa <146712422+iggyw1g@users.noreply.github.com>
2025-01-14 17:52:44 +0900

    update

--- components/library.tsx
+++ components/library.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbPlaylist } from "react-icons/tb";
+
 const Library = () => {
     const onClick = () => {
         //Handle upload later
@@ -15,6 +18,37 @@ const Library = () => {
             pt-4
             "
             >
+            <div
+            className="
+            inline-flex
+            items-center
+            gap-x-w
+            "
+            >
+                <TbPlaylist className = "text-neutral-400" size = {26}/>
+                <p
+                className ="
+                text-neutral-400
+                font-medium
+                text-md
+                " >
+                    Your Library
+                </p>
+            </div>
+            <AiOutlinePlus
+            onClick = {onClick}
+            size = {20}
+            className="
+            text-neutral-400
+            cursor-pointer
+            hover:text-white
+            transition
+            "
+            />
+            </div>
+            <div className= "flex flex-col gap-y-2 mt-4 px-3">
+                List of Songs!
+
             </div>
         </div>
     );
